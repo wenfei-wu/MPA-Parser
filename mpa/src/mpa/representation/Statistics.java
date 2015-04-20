a3bc1c571c531a843ee34dbedddf0108f8c3f105
wenfei-wu <wenfeiwu@cs.wisc.edu>
2015-04-20 18:57:50 -0500

    statistics

--- mpa/src/mpa/representation/Statistics.java
+++ mpa/src/mpa/representation/Statistics.java
@@ -5,6 +5,40 @@ import java.util.List;
 
 public class Statistics {
    List<String> interfaces = new ArrayList<String>();
+   List<String> acls = new ArrayList<String>();
+   List<Reference> references = new ArrayList<Reference>();
+   
+   public boolean hasUDLD;
+   public boolean hasMSTP;
+   public boolean hasDHCP;
+   public boolean hasDOT1Q;
+   public boolean hasHSRP;
+   public boolean hasLACP;
+   
+   public boolean hasOSPF;
+   public boolean hasBGP;
+   
+   
+   public int UDLDInst;
+   public int MSTPInst;
+   public int DHCPInst;
+   public int DOT1QInst;
+   public int HSRPInst;
+   public int LACPInst;
+   
+   public int OSPFInst;
+   public int BGPInst;
+   
+   public Statistics(){
+	   hasUDLD = false;  UDLDInst = 0;
+	   hasMSTP = false;  MSTPInst = 0;
+	   hasDHCP = false;  DHCPInst = 0;
+	   hasDOT1Q = false; DOT1QInst = 0;
+	   hasHSRP = false;  HSRPInst = 0;
+	   hasLACP = false;  LACPInst = 0;
+	   hasOSPF = false;  OSPFInst = 0;
+	   hasBGP = false;   BGPInst = 0;
+   }
    
    public void GetIface(String iface_name){
       interfaces.add(iface_name);
